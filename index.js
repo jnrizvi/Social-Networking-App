83e800734eed132eb71aee95b4345cfe7717481e
Jawad Rizvi <jawadrizvi40@gmail.com>
2020-12-11 11:26:21 -0700

    got a sample post from mongodb

--- index.js
+++ index.js
@@ -2,17 +2,33 @@ const { ApolloServer } = require("apollo-server");
 const gql = require('graphql-tag');
 const mongoose = require('mongoose');
 
+const Post = require('./models/Post.js');
+// const st = require('./models/Post.js') 
+
 const { MONGODB } = require('./config.js');
 
 const typeDefs = gql`
-    type Query {
-        sayHi: String!
+    type Post{
+        id: ID!
+        body: String!
+        createdAt: String!
+        username: String!
     }
-`
+    type Query {
+        getPosts: [Post]
+    }
+`;
 
 const resolvers = {
     Query: {
-        sayHi: () => 'Hello World!'
+        async getPosts(){
+            try{
+                const posts = await Post.find();
+                return posts;
+            } catch(err) {
+                throw new Error(err);
+            }
+        }
     }
 }
 
