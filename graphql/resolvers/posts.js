e6e9d53a6b984f1f55e87e33c03bf8959f51f66a
Jawad Rizvi <jawadrizvi40@gmail.com>
2020-12-12 20:35:28 -0700

    creating and deleting posts

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,16 +1,72 @@
 // Getting the post model
-const Post = require('../../models/Post.js')
+const { AuthenticationError } = require('apollo-server');
+
+const Post = require('../../models/Post.js');
+const checkAuth = require('../../util/check-auth.js');
 
 module.exports = {
     Query: {
         async getPosts(){
             try{
-                const posts = await Post.find();
+                const posts = await Post.find().sort({ createdAt: -1 });
                 return posts;
             } catch(err) {
                 throw new Error(err);
             }
+        },
+
+        async getPost(_, { postId }) {
+            try {
+                const post = await Post.findById(postId);
+
+                if (post) {
+                    return post
+                }
+                else {
+                    throw new Error('Post not found')
+                }
+            }
+            catch (err) {
+                throw new Error(err)
+            }
+        },
+    },
+
+    Mutation: {
+        async createPost(_, { body }, context) {
+            const user = checkAuth(context)
+            console.log(user);
+
+            const newPost = new Post({
+                body: body,
+                user: user.id,
+                username: user.username,
+                createdAt: new Date().toISOString()
+            });
+
+            const post = await newPost.save();
+
+            return post;
+        },
+
+        async deletePost(_, { postId }, context) {
+            // User can only delete their own post
+            const user = checkAuth(context);
+
+            try {
+                const post = await Post.findById(postId);
+
+                if (user.username === post.username) {
+                    await post.delete();
+                    return 'Post deleted successfully';
+                }
+                else {
+                    throw new AuthenticationError('Action not allowed')
+                }
+            }
+            catch(err) {
+                throw new Error(err)
+            }
         }
     }
-
 }
---
+++ util/check-auth.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken')
+const { AuthenticationError } = require('apollo-server')
+
+const { SECRET_KEY } = require('../config')
+
+module.exports = (context) => {
+    // context = { ...headers }
+    const authHeader = context.req.headers.authorization;
+    
+    // check if we have a token
+    if (authHeader) {
+        // Bearer ...
+        const token = authHeader.split('Bearer ')[1];
+        // check if we have a token
+        if (token) {
+            try {
+                const user = jwt.verify(token, SECRET_KEY);
+                return user
+            }
+            catch(err) {
+                throw new AuthenticationError('Invalid/Expired token');
+            }
+        }
+
+        throw new Error('Authentication token must be \'Bearer [token]')
+    }
+    throw new Error('Authorization header must be provided')
+}
