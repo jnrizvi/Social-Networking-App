a3e1ad8dd2c36fa442eff70ad98a6437c5320c8f
Jawad Rizvi <jawadrizvi40@gmail.com>
2020-12-13 20:27:33 -0700

    Setup react router and added semantic ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,26 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import { Container } from 'semantic-ui-react'
+
+import Home from './pages/Home.js';
+import Login from './pages/Login.js';
+import Register from './pages/Register.js';
+
+import 'semantic-ui-css/semantic.min.css'
 import './App.css';
+
+import MenuBar from './components/MenuBar.js'
 
 function App() {
     return (
-        <div>
-            <h1 >
-                Hello World!
-            </h1>
-        </div>
+        <Router>
+            <Container>
+                <MenuBar />
+                <Route exact path='/' component={Home}/>
+                <Route exact path='/login' component={Login}/>
+                <Route exact path='/register' component={Register}/>
+            </Container>
+        </Router>
     );
 }
 
